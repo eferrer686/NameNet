38b52906c36bebec25e35a9f77b0d6efcf56d3c3
Eduardo Ferrer <edfmgr@gmail.com>
2020-01-17 12:47:01 -0600

    Dataset csv

--- NamesData.py
+++ NamesData.py
@@ -1,4 +1,15 @@
 import pandas as pd
+from sklearn import preprocessing
+
 
 # Read SUR_NAMES DB
 db = pd.read_csv("data/Common_surnames.csv")
+
+db = db.loc[:,['name','prop100k']]
+
+print(db)
+
+prop = db.values #returns a numpy array
+min_max_scaler = preprocessing.MinMaxScaler()
+x_scaled = min_max_scaler.fit_transform(db['prop100k'])
+db['prop100k'] = x_scaled
